4a6e1a248e9d6d37ce6674457267f9a405409268
anirudhkala110 <anirudhkala110@gmail.com>
2024-06-13 01:29:00 +0530

    Updated Data

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -76,7 +76,12 @@ const Projects = () => {
                                     <div className="portfolio_box">
                                         <p className='text-white'>Small Robo with audio and dancing mode.</p>
                                         <div className="btn_wrapper">
-                                            <a href="/projects" className="text-decoration-none">Read More<i className="fa-solid fa-angle-right"></i></a>
+                                            <button className="btn btn-link collapsed text-decoration-none text-white" data-toggle="collapse" data-target="#collapseSix0" aria-expanded="false" aria-controls="collapseSix0">Read More<i className="fa-solid fa-angle-right"></i></button>
+                                            <div id="collapseSix0" className="collapse bg-dark px-2" aria-labelledby="headingSix" style={{ position: "absolute", zIndex: '600', maxHeight: '250px', overflow: 'auto' }}>
+                                                <p>
+                                                    Discover our latest project: a Small Robo equipped with audio and dancing modes! This engaging robot can play your favorite tunes while showcasing its impressive dance moves. Designed for entertainment and education, it’s perfect for kids and tech enthusiasts alike. With intuitive controls and interactive features, our Small Robo brings fun and learning together. Watch it groove to the beat and add a lively touch to any room. Explore the future of robotics with our innovative and entertaining creation!
+                                                </p>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -87,11 +92,18 @@ const Projects = () => {
                                 <div className="portfolio_image">
                                     <figure className="mb-0 rounded-4" style={{ height: '400px', background: '' }}><img src={clock} alt="" className="img-fluid" style={{ height: '400px' }} /></figure>
                                     <div className="portfolio_box">
-                                        <p>3D Clock with best sound effects and with 360<sup>o</sup> view.</p>
+                                        <p>3D Clock with best alarm and with 360<sup>o</sup> view.</p>
                                         <div className="btn_wrapper">
-                                            <a href="/projects" className="text-decoration-none">Read More<i className="fa-solid fa-angle-right"></i></a>
+                                            <button className="btn btn-link collapsed text-decoration-none text-white" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">Read More<i className="fa-solid fa-angle-right"></i></button>
+                                            <div id="collapseSix" className="collapse bg-dark px-2" aria-labelledby="headingSix" style={{ position: "absolute", zIndex: '600', maxHeight: '250px', overflow: 'auto' }}>
+                                                <p>
+                                                    This 3D clock application features an advanced alarm system and a 360-degree view. The holographic display projects a clear, interactive clock above its base. The superior alarm functionality ensures reliable wake-ups, while the 360-degree perspective provides a modern, engaging way to keep track of time from any angle.
+                                                </p>
+                                            </div>
                                         </div>
+
                                     </div>
+
                                     <div className="portfolio_image_box">
                                         <span>3D Technology</span>
                                     </div>
@@ -105,7 +117,12 @@ const Projects = () => {
                                     <div className="portfolio_box">
                                         <p>Automatic delivery with all safety majors.</p>
                                         <div className="btn_wrapper">
-                                            <a href="/projects" className="text-decoration-none">Read More<i className="fa-solid fa-angle-right"></i></a>
+                                            <button className="btn btn-link collapsed text-decoration-none text-white" data-toggle="collapse" data-target="#collapseSix1" aria-expanded="false" aria-controls="collapseSix1">Read More<i className="fa-solid fa-angle-right"></i></button>
+                                            <div id="collapseSix1" className="collapse bg-dark px-2 rounded" aria-labelledby="headingSix" style={{ position: "absolute", zIndex: '600', maxHeight: '250px', overflow: 'auto' }}>
+                                                <p>
+                                                    This automatic delivery robot is controlled via a mobile application. Users can input the delivery location through the app, and the robot will navigate to the specified destination. Equipped with advanced safety measures and efficient navigation systems, it provides a reliable and modern solution for contactless deliveries. In the medical field, it can be used to transport medications, lab samples, and medical supplies within hospitals or to patients' homes, ensuring timely and secure delivery. This reduces the risk of contamination and enhances efficiency in healthcare logistics.
+                                                </p>
+                                            </div>
                                         </div>
                                     </div>
                                     <div className="portfolio_image_box">
@@ -120,7 +137,12 @@ const Projects = () => {
                                 <div className="portfolio_box">
                                     <p>Automatic detection and security drone.</p>
                                     <div className="btn_wrapper">
-                                        <a href="/projects" className="text-decoration-none">Read More<i className="fa-solid fa-angle-right"></i></a>
+                                        <button className="btn btn-link collapsed text-decoration-none text-white" data-toggle="collapse" data-target="#collapseSix2" aria-expanded="false" aria-controls="collapseSix2">Read More<i className="fa-solid fa-angle-right"></i></button>
+                                        <div id="collapseSix2" className="collapse bg-dark px-2 rounded" aria-labelledby="headingSix" style={{ position: "absolute", zIndex: '600', maxHeight: '250px', overflow: 'auto' }}>
+                                            <p>
+                                                Our cutting-edge automatic detection and security drone offers unparalleled surveillance and protection. Designed for both residential and commercial use, this drone features advanced AI-powered sensors that detect and respond to potential threats in real-time. With high-definition cameras and night vision capabilities, it provides continuous monitoring, ensuring safety around the clock. Controlled via a user-friendly mobile application, users can receive instant alerts, live video feeds, and detailed reports directly on their devices. Enhance your security infrastructure with our state-of-the-art drone, delivering peace of mind through innovative technology.
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
                                 <div className="portfolio_image_box">
@@ -134,7 +156,12 @@ const Projects = () => {
                                 <div className="portfolio_box">
                                     <p>Smart Water Tank. Get All updates on your mobile phone.</p>
                                     <div className="btn_wrapper">
-                                        <a href="/projects" className="text-decoration-none">Read More<i className="fa-solid fa-angle-right"></i></a>
+                                        <button className="btn btn-link collapsed text-decoration-none text-white" data-toggle="collapse" data-target="#collapseSix3" aria-expanded="false" aria-controls="collapseSix3">Read More<i className="fa-solid fa-angle-right"></i></button>
+                                        <div id="collapseSix3" className="collapse bg-dark px-2 rounded" aria-labelledby="headingSix" style={{ position: "absolute", zIndex: '600', maxHeight: '250px', overflow: 'auto' }}>
+                                            <p>
+                                                Experience the convenience of our Smart Water Tank, designed to keep you informed and in control. This innovative system monitors water levels, usage, and quality, sending real-time updates directly to your mobile phone. With its user-friendly app, you can track consumption, receive low-level alerts, and schedule maintenance effortlessly. Ensure efficient water management and prevent wastage with our state-of-the-art Smart Water Tank. Stay connected and make smarter decisions for your home or business, all from the palm of your hand.
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
                                 <div className="portfolio_image_box">
@@ -148,7 +175,12 @@ const Projects = () => {
                                 <div className="portfolio_box">
                                     <p>Auto Dustbin for dry and wet wastage.</p>
                                     <div className="btn_wrapper">
-                                        <a href="/projects" className="text-decoration-none">Read More<i className="fa-solid fa-angle-right"></i></a>
+                                        <button className="btn btn-link collapsed text-decoration-none text-white" data-toggle="collapse" data-target="#collapseSix4" aria-expanded="false" aria-controls="collapseSix4">Read More<i className="fa-solid fa-angle-right"></i></button>
+                                        <div id="collapseSix4" className="collapse bg-dark px-2 rounded" aria-labelledby="headingSix" style={{ position: "absolute", zIndex: '600', maxHeight: '250px', overflow: 'auto' }}>
+                                            <p>
+                                                Introducing our innovative Auto Dustbin, designed to streamline waste management for both dry and wet waste. This advanced dustbin features a sensor-operated lid, ensuring a hygienic and touch-free experience. Separate compartments for dry and wet waste make recycling and disposal more efficient. With a sleek design and durable construction, our Auto Dustbin is perfect for homes, offices, and public spaces. Enhance your waste disposal system today with our state-of-the-art Auto Dustbin, combining convenience, cleanliness, and environmental responsibility.
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
                                 <div className="portfolio_image_box">
@@ -163,8 +195,10 @@ const Projects = () => {
                 </figure>
             </section >
             {/* <!--STATISTICS SECTION-- > */}
-            < section >
-                <Statics />
+            < section className='mt-5 pt-5' >
+                <div className='mt-5 pt-5'>
+                    <Statics className="" />
+                </div>
             </section >
         </div >
     )
