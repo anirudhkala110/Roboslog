69a8f7a656a337f5c43dfdbf3b526e8a75cce23b
Anirudh Kala <122237860+anirudhkala110@users.noreply.github.com>
2024-06-15 11:55:56 +0530

    Update Footer.jsx for gmail

--- src/Utils/Footer.jsx
+++ src/Utils/Footer.jsx
@@ -107,7 +107,7 @@ const Footer = () => {
                                         </li>
                                         <li class="contact_mail">
                                             <i class="fa-sharp fa-solid fa-envelope"></i>
-                                            <a href="mailto:Info@immersiveai.com" class="text-decoration-none">roboslog@gmail.com</a>
+                                            <a href="mailto:roboslog@gmail.com" class="text-decoration-none">roboslog@gmail.com</a>
                                         </li>
                                         <li class="mb-0">
                                             <a className='d-flex' href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d35661.6942074034!2d144.91637077397664!3d-37.811636671563036!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad65d4c2b349649%3A0xb6899234e561db11!2sEnvato!5e0!3m2!1sen!2s!4v1659530976839!5m2!1sen!2s" target='__blank' >
@@ -129,7 +129,7 @@ const Footer = () => {
                                             </li>
                                             <li class="contact_mail">
                                                 <i class="fa-sharp fa-solid fa-envelope"></i>
-                                                <a href="mailto:Info@immersiveai.com" class="text-decoration-none">roboslog@gmail.com</a>
+                                                <a href="mailto:roboslog@gmail.com" class="text-decoration-none">roboslog@gmail.com</a>
                                             </li>
                                         </ul>
                                         <ul class="list-unstyled mb-0">
